afbdd3ebfdde28160f7755831a0cd945b2a306e9
yutianzlucy <yutianz@bu.edu>
2024-12-03 20:14:52 -0500

    final
    final

--- src/js/character.js
+++ src/js/character.js
@@ -5,12 +5,14 @@ const data = {
         { "audio": "sounds/xavier3.mp3", "text": "You need to learn how to take responsibility for your actions." }
     ],
     "rafayel": [
-        { "audio": "sounds/rafayel1.mp3", "text": "welcome" },
-        { "audio": "sounds/rafayel2.mp3", "text": "see you" }
+        { "audio": "sounds/rafayel1.mp3", "text": "Many paths are worthed exploring, even have they all must end." },
+        { "audio": "sounds/rafayel2.mp3", "text": "You always insist on touching my face." },
+        { "audio": "sounds/rafayel3.mp3", "text": "Did you eat all the snacks we bought?" }
     ],
     "erik": [
-        { "audio": "erik1.mp3", "text": "hey there" },
-        { "audio": "erik2.mp3", "text": "bye now" }
+        { "audio": "sounds/erik1.mp3", "text": "Did you detect anomalous with my heartbeat?" },
+        { "audio": "sounds/erik2.mp3", "text": "Aren't you tired of being in that position? You can come a little closer." },
+        { "audio": "sounds/erik3.mp3", "text": "If you don't want to choose, we can decide through a game of rock paper scissors, do you want to represent hotpot or barbecue?" }
     ]
 };
 
@@ -23,11 +25,12 @@ console.log(characterId)
 // Create the HTML structure
 document.body.innerHTML = `
     <audio id="audioPlayer"></audio>
+    <p class="Touch">Touch him!</p>
 
     <div class="background${characterId}-image">
     </div>
     <div class="character-center">
-        <img src="images/${characterId}.png" alt="${characterId}" class="character-image" id="${characterId}">
+        <img src="images/${characterId}.png" alt="${characterId}" class="characters-image" id="${characterId}">
         <div class="chat-container" id="chat-container"></div>
     </div>
     <div class="aside">
@@ -59,28 +62,28 @@ const handleClick = () => {
     characterElement.removeEventListener('click', handleClick);
 
     if (favorabilityHeight < maxFavorabilityHeight) {
-        favorabilityHeight += maxFavorabilityHeight*1;
+        favorabilityHeight += maxFavorabilityHeight*0.2;
         if (favorabilityHeight > maxFavorabilityHeight) {
             favorabilityHeight = maxFavorabilityHeight;
         }
         currentFavorabilityBar.style.height = `${favorabilityHeight}px`;
     }
 
-    if (favorabilityHeight == maxFavorabilityHeight) {
-        const characterCenter = document.querySelector(`.character-center`);
+   // if (favorabilityHeight == maxFavorabilityHeight) {
+       // const characterCenter = document.querySelector(`.character-center`);
         
         // const heart = document.createElement('div');
         // heart.textContent = '❤️';
-        const heart = document.createElement('img');
-        heart.src = `images/${characterId}Q.png`;
-        heart.classList.add('heart-animation');
+        //const heart = document.createElement('img');
+       // heart.src = `images/heart.jpg`;
+       // heart.classList.add('heart-animation');
         
-        characterCenter.appendChild(heart);
+       // characterCenter.appendChild(heart);
         
-        setTimeout(() => {
-            characterCenter.removeChild(heart);
-        }, 2000);
-    }
+       // setTimeout(() => {
+           // characterCenter.removeChild(heart);
+       // }, 2000);
+   // }
 
     audio.play();
 
